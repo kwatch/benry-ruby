524e90c5076d4e2f1f4c5b8ac4527979c83e1df0
kwatch <kwatch@gmail.com>
2023-05-16 13:39:39 +0900

    [ruby] cmdapp: update 'Rakefile.rb'

--- benry-cmdapp/Rakefile.rb
+++ benry-cmdapp/Rakefile.rb
@@ -6,17 +6,16 @@ RELEASE   = ENV['RELEASE'] || "0.0.0"
 COPYRIGHT = "copyright(c) 2023 kuwata-lab.com all rights reserved"
 LICENSE   = "MIT License"
 
-
-def run_test(ruby=nil, &b)
-  if ruby
-    #sh "#{ruby} test/cmdapp_test.rb", &b      # for MiniTest
-    sh "#{ruby} test/cmdapp_test.rb -sc", &b   # for Oktest
-  else
-    #ruby "test/cmdapp_test.rb", &b            # for MiniTest
-    sh "oktest test/cmdapp_test.rb -sc", &b    # for Oktest
-  end
-end
-
 RUBY_VERSIONS = ["3.2", "3.1", "3.0", "2.7", "2.6", "2.5", "2.4", "2.3"]
 
 Dir.glob('./task/*-task.rb').each {|x| require x }
+
+def run_test(ruby=nil, &b)
+  if ruby
+    #sh "#{ruby} test/run_all.rb", &b      # for MiniTest
+    sh "#{ruby} test/run_all.rb -sc", &b   # for Oktest
+  else
+    #ruby "test/run_all.rb", &b            # for MiniTest
+    sh "oktest test/run_all.rb -sc", &b    # for Oktest
+  end
+end
