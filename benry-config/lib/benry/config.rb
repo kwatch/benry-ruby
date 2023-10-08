072afb87f797756276c2f4aa34b5b220da52fb77
kwatch <kwatch@gmail.com>
2023-10-08 19:32:23 +0900

    [config] remove sample code from doc comment

--- benry-config/lib/benry/config.rb
+++ benry-config/lib/benry/config.rb
@@ -17,49 +17,7 @@ module Benry
   ##
   ## Configuration class.
   ##
-  ## ex:
-  ##
-  ##     #----- config/common.rb -----
-  ##     require 'benry/config'
-  ##     class CommonConfig < Benry::BaseConfig
-  ##       add :db_user            , "user1"
-  ##       add :db_pass            , ABSTRACT
-  ##       add :session_cooie      , "SESS"
-  ##       ## or:
-  ##       #add :session_secret    , SECRET['SESSION_SECRET']  # get secret value from ENV
-  ##     end
-  ##
-  ##     #----- config/development.rb -----
-  ##     require 'config/common'
-  ##     class Config < CommonConfig
-  ##       set :db_pass            , "pass1"
-  ##     end
-  ##
-  ##     #----- config/development.private -----
-  ##     class Config
-  ##       set :session_secret     , "YRjCIAiPlCBvwLUq5mnZ"
-  ##     end
-  ##
-  ##     #----- main.rb -----
-  ##     rack_env = ENV['RACK_ENV']  or raise "$RACK_ENV required."
-  ##     require "./config/#{rack_env}.rb"
-  ##     load    "./config/#{rack_env}.private"
-  ##     #
-  ##     $config = Config.new.freeze
-  ##     #
-  ##     p $config.db_user             #=> "user1"
-  ##     p $config.db_pass             #=> "pass1"
-  ##     p $config.session_cookie      #=> "SESS"
-  ##     p $config.session_secret      #=> "abc123"
-  ##     #
-  ##     p $config.get_all(:db_)       #=> {:user=>"user1", :pass=>"pass1"}
-  ##     p $config.get_all(:session_)  #=> {:cookie=>"SESS", :secret=>"YRjCIAiPlCBvwLUq5mnZ"}
-  ##     #
-  ##     p $config.defined?(:db_pass)      #=> true
-  ##     p $config.defined?(:db_password)  #=> false
-  ##     #
-  ##     $config.each  {|k, v| puts "#{k}=#{v.inspect}" }   # hide secret values as "(secret)"
-  ##     $config.each! {|k, v| puts "#{k}=#{v.inspect}" }   # not hide secret values
+  ## https://github.com/kwatch/benry-ruby/tree/main/benry-config#readme
   ##
   class BaseConfig
 
