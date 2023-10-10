d2aa7908f5ee6c9202630f535bee0105b931b141
kwatch <kwatch@gmail.com>
2023-10-10 22:22:54 +0900

    [cmdopt] tweak 'Rakefile.rb'

--- benry-cmdopt/Rakefile.rb
+++ benry-cmdopt/Rakefile.rb
@@ -8,4 +8,4 @@ LICENSE   = "MIT License"
 
 #RUBY_VERSIONS = ["3.2", "3.1", "3.0", "2.7", "2.6", "2.5", "2.4", "2.3"]
 
-Dir.glob('./task/*-task.rb').each {|x| require x }
+Dir.glob('./task/*-task.rb').sort.each {|x| require x }
