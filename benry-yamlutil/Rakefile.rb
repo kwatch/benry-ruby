20bd3518620791d6e2c4f7ae7b5c736ef2568cc5
kwatch <kwatch@gmail.com>
2023-10-11 16:04:08 +0900

    [yamlutil] update 'Rakefile.rb'

--- benry-yamlutil/Rakefile.rb
+++ benry-yamlutil/Rakefile.rb
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 
-PROJECT   = "benry-yamlutl"
+PROJECT   = "benry-yamlutil"
 RELEASE   = ENV['RELEASE'] || "0.0.0"
 COPYRIGHT = "copyright(c) 2016 kwatch@gmail.com"
 LICENSE   = "MIT License"
 
 #RUBY_VERSIONS = ["3.2", "3.1", "3.0", "2.7", "2.6", "2.5", "2.4", "2.3"]
 
-Dir.glob('./task/*-task.rb').each {|x| require x }
+Dir.glob('./task/*-task.rb').sort.each {|x| require x }
