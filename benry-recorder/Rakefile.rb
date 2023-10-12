a8938acfa1caecd9a219686dce49ffcc86fe145e
kwatch <kwatch@gmail.com>
2023-10-13 07:11:36 +0900

    [recorder] fix 'Rakefile.rb' to run minitest instead of oktest

--- benry-recorder/Rakefile.rb
+++ benry-recorder/Rakefile.rb
@@ -10,6 +10,12 @@ README_EXTRACT  = /^file: (.*\.rb)/
 
 Dir.glob("./task/*.rb").sort.each {|x| require_relative x }
 
+def run_test(ruby=nil, &b)
+  ## benry-recorder is depended by Oktest, therefore benry-recorder
+  ## should NOT adopt Oktest. Use MiniTest instead.
+  run_minitest(ruby, &b)
+end
+
 def readme_extract_callback(filename, str)
   if filename == 'example1.rb'
     str =~ /class Calc\n(.*?)^end\n/m
