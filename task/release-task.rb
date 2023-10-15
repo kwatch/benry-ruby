bbf3e7f05fbcad158ad5db75ee794661d1c5ddf9
kwatch <kwatch@gmail.com>
2023-10-15 13:10:41 +0900

    [main] task: define 'release' task

--- task/release-task.rb
+++ task/release-task.rb
@@ -5,6 +5,72 @@ defined? PROJECT    or abort "PROJECT required."
 defined? RELEASE    or abort "RELEASE required."
 
 
+desc "start release operation interactively"
+task :release do
+  do_release()
+end
+
+def do_release()
+  RELEASE != '0.0.0'  or abort "** ERROR: 'RELEASE=X.X.X' required."
+  commands = []
+  guide_message(PROJECT, RELEASE).scan(/^  \$ (.*)/) do |command,|
+    commands << command
+  end
+  run_shell_commands(commands)
+end
+
+def run_shell_commands(commands)
+  dirs = []
+  commands.each do |command|
+    optional = command =~ /\# optional/
+    y_n = optional ? "y/N" : "Y/n"
+    puts ""
+    puts "\e[34m$ #{command}\e[0m"
+    print "\e[2m? [#{y_n}/!(command)/q(uit)]>\e[0m "
+    answer = $stdin.gets().strip()
+    if answer.empty?
+      answer = optional ? "N" : "Y"
+    end
+    case answer
+    when /^[Yy]/
+      case command
+      when /^cd -( +|$)/
+        dir = dirs.pop()
+        puts "cd -\t\t# #{dir}"
+        Dir.chdir dir
+      when /^cd (\S+)/
+        dir = $1
+        dirs.push(Dir.pwd)
+        puts "cd #{dir}"
+        Dir.chdir dir
+      else
+        sh command
+      end
+    when /^[Nn]/
+      next
+    when /^[!](.*)/
+      cmd = $1.strip()
+      if cmd.empty?
+        print "\e[2menter command: \e[0m"
+        cmd = $stdin.gets().strip()
+      end
+      if !cmd.empty?
+        #sh cmd
+        puts cmd
+        system cmd
+      end
+      redo
+    when /^[Qq]/
+      break
+    else
+      puts "\e[31mUnexpected answer. Retry.\e[0m"
+      redo
+    end
+  end
+  nil
+end
+
+
 desc "show release operations"
 task :'release:guide' do
   do_release_guide()
