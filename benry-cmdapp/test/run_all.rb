cead22164b0a16154c06e50705fa3a05c0755f52
kwatch <kwatch@gmail.com>
2023-10-20 00:50:06 +0900

    [cmdapp] delete 'lib/*' and 'test/*'

--- benry-cmdapp/test/run_all.rb
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-Dir.glob(File.dirname(__FILE__) + '/*.rb').each do |filename|
-#$stderr.puts "\033[0;31m*** debug: filename=#{filename.inspect}\033[0m"
-  require_relative File.basename(filename)
-end
